177ea0af02b502132d2b926b3147832fa2af967b
Aluno <aluno@etec.sp.gov.br>
2023-02-08 19:21:53 -0300

    Meu primeiro modulo em node.

---
+++ Aula0602/index.js
@@ -0,0 +1,7 @@
+const meuModulo = require('./soma.js')
+const calculadora = meuModulo.calculadora
+
+calculadora(26, 52, "+")
+calculadora(50, 24, "-")
+calculadora(4, 24, "*")
+calculadora(60, 5, "-")
